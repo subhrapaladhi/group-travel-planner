f86adb11b4d452665047526f6d8b85da85075353
subhrapaladhi <subhrapaladhi9@gmail.com>
2019-11-02 22:26:17 +0530

    backend fully working :)

--- routes/index.js
+++ routes/index.js
@@ -13,61 +13,40 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>{
   User.findById(req.user._id)
       .then(async(data)=>{
         let plansData = new Array();
-        // if(data.plans.length==0){
-        //   res.render('dashboard', {
-        //     user: req.user,
-        //     plans: plansData
-        //   })
-        // }else{
-        //   data.plans.forEach(async(planid,index)=>{
-        //     await Plan.findById(planid)
-        //         .then((pdata)=>{
-        //             plansData.push(pdata)
-        //             if(index==data.plans.length-1){
-        //               res.render('dashboard', {
-        //                 user: req.user,
-        //                 plans: plansData
-        //               })
-        //             }
-        //         })
-        //   })  
-        // }
-
-        Plan.find
+        plansData = await Plan.find().where('_id').in(data.plans).exec();
+        console.log(plansData)
+        res.render('dashboard', {
+          user: req.user,
+          plans: plansData
+        })
       })
       .catch((err)=>{
         console.log(err)
       })
-
-  User.find()
-  res.send
 });
 
 router.get('/newplan', ensureAuthenticated,(req, res)=>{
-  let temp = new Array();
   res.render('createnew', {
-    user: req.user,              // create newplan.ejs to render form for a new plan
+    user: req.user
   })                         
 })
 
 router.post('/newplan', ensureAuthenticated, (req, res)=>{
-  req.body["members"]=[req.user._id]
-  console.log('req.user====', req.user)
+  console.log('req.body======',req.body)
   new Plan(req.body)
       .save()
       .then((plan)=>{
-        console.log('plan ==== ', plan)
+        console.log('plan=====',plan)
         let userData = req.user;
         userData.plans.push(plan._id)
         User.findByIdAndUpdate(req.user._id, userData, {new: true})
             .then((data)=>{
-              console.log('data===',data)
+              res.redirect('/dashboard');
             })
             .catch((err)=>{
               console.log(err)
             })
       })
-  res.redirect('/dashboard')
 })
 
 router.get('/showDetails/:id',ensureAuthenticated,(req, res)=>{
